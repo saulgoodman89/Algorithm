d4c70cdbe3d43c19e232c46d4975df5e96df0017
EungGyo <eunggyo@hutek.net>
2024-05-03 09:32:31 +0900

    24.05.03 14681 끝

--- 조건문/14681/main.c
+++ 조건문/14681/main.c
@@ -14,6 +14,21 @@
 */
 
 int main() {
+	int x , y = 0;
+	scanf("%d",&x);
+	scanf("%d",&y);
 
-
+	if((-1000 <= x <= 1000 && x!=0) && (-1000<=y<=1000 && y!=0)) {
+		if(x>0 && y>0) {
+			printf("1");
+		}else if(x<0 && y>0) {
+			printf("2");
+		}else if(x<0 && y<0) {
+			printf("3");
+		}else if(x>0 && y<0) {
+			printf("4");
+		}
+	}else {
+		printf("Not Valid Number");
+	}
 }
